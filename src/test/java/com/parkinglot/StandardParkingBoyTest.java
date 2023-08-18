7e24f719251e38696fb8509d0a4212e577aaaedb
AubBantolo <jhuliet14@gmail.com>
2023-08-18 22:47:29 +0800

    feat: added test return_fetched_cars_when_parking_boy_fetch_the_two_cars_given_parked_with_corresponding_tickets

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -33,4 +33,24 @@ class StandardParkingBoyTest {
         //Then
         assertEquals(car, fetchedCar);
     }
+
+    @Test
+    void should_return_fetched_cars_when_parking_boy_fetch_the_two_cars_given_parked_with_corresponding_tickets() {
+        //Given
+        Car car1 = new Car();
+        Car car2 = new Car();
+        StandardParkingBoy standardParkingBoy = new StandardParkingBoy();
+        ParkingTicket parkingTicket1 = standardParkingBoy.parkCar(car1);
+        ParkingTicket parkingTicket2 = standardParkingBoy.parkCar(car2);
+
+        //When
+        Car fetchedCar1 = standardParkingBoy.fetchCar(parkingTicket1);
+        Car fetchedCar2 = standardParkingBoy.fetchCar(parkingTicket2);
+
+        //Then
+        assertSame(car1, fetchedCar1);
+        assertSame(car2, fetchedCar2);
+    }
+
+  
 }
